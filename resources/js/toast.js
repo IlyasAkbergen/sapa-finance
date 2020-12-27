ae155329d45cf1a38575c1ce8878d112adf41426
Meirambek <yensepbay@gmail.com>
2020-12-27 17:52:58 +0600

    добавлено: уведомление об ошибках

---
+++ resources/js/toast.js
@@ -0,0 +1,21 @@
+import 'izitoast/dist/css/iziToast.min.css'
+import iZtoast from 'izitoast'
+
+const toast = {
+    error: (message = "Данные не сохранены", title = 'Ошибка') => {
+        return iZtoast.error({
+            title: title,
+            message: message,
+            position: 'bottomRight',
+        })
+    },
+    success: (message= 'Ваши данные сохранены', title = 'Успешно') => {
+        return iZtoast.success({
+            title: title,
+            message: message,
+            position: 'bottomRight',
+        })
+    },
+}
+
+export default toast
