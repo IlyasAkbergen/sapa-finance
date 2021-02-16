b8e91302430c133dac205328ebfef0bed2c0df7b
ilyasakbergen <ilijas9813@gmail.com>
2021-02-17 00:29:02 +0600

    fix

--- resources/js/Pages/Homework/Item.vue
+++ resources/js/Pages/Homework/Item.vue
@@ -1,33 +1,33 @@
 <template>
   <tr class="clickable">
     <th class="float-left" scope="row" @click.self="() => $emit('show')">
-      <inertia-link :href="route('users.show', homework?.user?.id)">
-        {{ homework?.user?.name }}
+      <inertia-link :href="route('users.show', homework.user.id)" v-if="homework.user">
+        {{ homework.user ? homework.user.name : 'Неизвестно' }}
       </inertia-link>
     </th>
     <td class="align-middle" @click.stop="() => $emit('show')">
-      {{ homework?.lesson?.title }}
+      {{ homework.lesson ? homework.lesson.title : 'Неизвестно' }}
     </td>
     <td class="float-right">
       <a class="osk__action osk__action--sky"
-         v-if="homework?.status !== 1"
+         v-if="homework.status !== 1"
          @click.prevent="accept"
          href="#">
         Принять
       </a>
       <a class="osk__action osk__action--red"
-         v-if="homework?.status !== 2"
+         v-if="homework.status !== 2"
          @click.prevent="reject"
          href="#">
         Отказать
       </a>
     </td>
 
-    <td class="float-right" v-if="homework?.status === 2" @click.stop="() => $emit('show')">
+    <td class="float-right" v-if="homework.status === 2" @click.stop="() => $emit('show')">
       <a class="osk__status osk__status--red">Отказано</a>
     </td>
 
-    <td class="float-right" v-if="homework?.status === 1" @click.stop="() => $emit('show')">
+    <td class="float-right" v-if="homework.status === 1" @click.stop="() => $emit('show')">
       <a class="osk__status osk__status--sky">Принято</a>
     </td>
   </tr>
