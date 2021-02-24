8af4314d9ddde83e521911924c8ab51aedcd4e9f
ilyasakbergen <ilijas9813@gmail.com>
2021-02-25 01:08:23 +0600

    fix

--- app/Mail/HomeworkCreated.php
+++ app/Mail/HomeworkCreated.php
@@ -7,9 +7,11 @@ namespace App\Mail;
 use App\Models\Homework;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class HomeworkCreated
+class HomeworkCreated extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
